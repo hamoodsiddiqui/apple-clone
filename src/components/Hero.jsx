c0bd6849d8ab0054160c0879d4395dc87c4cf75c
hamoodsiddiqui <hamoodsiddique@gmail.com>
2024-08-13 18:46:01 +0500

    video section done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
         <p id="hero" className="hero-title">
-          iPhone 15 Pro
+          آئی فون 15 پرو
         </p>
         <div className="md:w-10/12 w-9/12">
           <video
