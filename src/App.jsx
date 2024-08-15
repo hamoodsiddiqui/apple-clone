43982e74b28a849b0be7e3d74d75468e47938b90
hamoodsiddiqui <hamoodsiddique@gmail.com>
2024-08-15 13:10:32 +0500

    started model component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import Model from "./components/Model";
 import "./App.css";
 
 const App = () => {
   return (
     <main className="bg-black">
-      <Navbar /> <Hero /> <Highlights />
+      <Navbar /> <Hero /> <Highlights /> <Model />
     </main>
   );
 };
